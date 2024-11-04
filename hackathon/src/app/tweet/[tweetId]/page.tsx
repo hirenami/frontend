cbed61e483793d929e6b516692865a609ac124ea
Namito Hirezaki <7310wave@gmail.com>
2024-11-04 16:30:23 +0900

    update

--- hackathon/src/app/tweet/[tweetId]/page.tsx
+++ hackathon/src/app/tweet/[tweetId]/page.tsx
@@ -4,7 +4,7 @@ import Sidebar from "@/components/pages/sidebar";
 import TrendsSidebar from "@/components/pages/trendsidebar";
 import { useParams } from "next/navigation";
 import React, { useState, useEffect } from "react";
-import { Tweet,User } from "@/types";
+import { Tweet, User } from "@/types";
 import { fetchOneTweet } from "@/features/tweet/fetchOneTweet";
 import { onAuthStateChanged } from "firebase/auth";
 import { fireAuth } from "@/features/firebase/auth";
@@ -15,54 +15,69 @@ import { createLike, deleteLike } from "@/features/like/likes";
 import { createRetweet, deleteRetweet } from "@/features/retweet/retweets";
 import { Avatar, AvatarImage, AvatarFallback } from "@/components/ui/avatar";
 import { Button } from "@/components/ui/button";
-import { MoreHorizontal, MessageCircle, Repeat, Heart, BarChart } from "lucide-react";
+import {
+    MoreHorizontal,
+    MessageCircle,
+    Repeat,
+    Heart,
+    BarChart,
+} from "lucide-react";
 import { renderContentWithHashtags } from "@/lib/renderContentWithHashtags";
-import { formatDate } from "@/lib/formatDate";
 import RetweetItem from "@/components/pages/retweetItems";
-import  Image from "next/image";
+import Image from "next/image";
+import { useRouter } from "next/navigation";
+import { ArrowLeft } from "lucide-react";
 
 export default function TweetPage() {
     const { tweetId } = useParams();
     const tweetid = tweetId as unknown as number;
     const [tweet, setTweet] = useState<Tweet | null>(null);
-	const [user, setUser] = useState<User | null>(null);
+    const [user, setUser] = useState<User | null>(null);
     const [isliked, setIsLiked] = useState<boolean>(false);
     const [likeData, setLikeData] = useState<number>(0);
-	const [isretweet, setIsRetweet] = useState<boolean>(false);
-	const [retweetData, setRetweetData] = useState<number>(0);
+    const [isretweet, setIsRetweet] = useState<boolean>(false);
+    const [retweetData, setRetweetData] = useState<number>(0);
     const [retweet, setRetweet] = useState<Tweet | null>(null);
-	const auth = fireAuth
+    const auth = fireAuth;
+    const router = useRouter();
 
     useEffect(() => {
-		if (!tweetId) return;
-		const unsubscribe = onAuthStateChanged(auth, async (user) => {
-			if (user) {
-				try {
-					const token = await user.getIdToken();
-					const tweetData = await fetchOneTweet(token,tweetid);
-					setTweet(tweetData);
-					await Promise.all([
-						setUser(await fetchUserData(token, tweetData.userid)),
-						setIsLiked(await fetchLikeStatus(token, tweetData.tweetid)),
-						setIsRetweet(await fetchRetweetStatus(token, tweetData.tweetid)),
-					]);
-					if (tweetData.retweetid.Valid) {
-                        setRetweet(await fetchOneTweet(token,tweetData.retweetid.Int32));
+        if (!tweetId) return;
+        const unsubscribe = onAuthStateChanged(auth, async (user) => {
+            if (user) {
+                try {
+                    const token = await user.getIdToken();
+                    const tweetData = await fetchOneTweet(token, tweetid);
+                    setTweet(tweetData);
+                    await Promise.all([
+                        setUser(await fetchUserData(token, tweetData.userid)),
+                        setIsLiked(
+                            await fetchLikeStatus(token, tweetData.tweetid)
+                        ),
+                        setIsRetweet(
+                            await fetchRetweetStatus(token, tweetData.tweetid)
+                        ),
+                    ]);
+                    if (tweetData.retweetid.Valid) {
+                        setRetweet(
+                            await fetchOneTweet(
+                                token,
+                                tweetData.retweetid.Int32
+                            )
+                        );
                     }
-					setLikeData(tweetData.likes);
-		setRetweetData(tweetData.retweets);
+                    setLikeData(tweetData.likes);
+                    setRetweetData(tweetData.retweets);
                 } catch (error) {
                     console.error("ユーザーがログインしていません", error);
                 }
-            return unsubscribe;
-        };
-        unsubscribe();
-	}
-		);
-	}, [auth, tweetid, tweetId]);
-		
+                return unsubscribe;
+            }
+            unsubscribe();
+        });
+    }, [auth, tweetid, tweetId]);
 
-	const hundleUserClick = async () => {
+    const hundleUserClick = async () => {
         try {
             const token = await auth.currentUser?.getIdToken();
             if (token) {
@@ -100,7 +115,7 @@ export default function TweetPage() {
         }
     };
 
-	const handleRetweetToggle = async () => {
+    const handleRetweetToggle = async () => {
         try {
             const token = await auth.currentUser?.getIdToken();
             if (token && tweet) {
@@ -127,154 +142,171 @@ export default function TweetPage() {
         }
     };
 
-	const Tweetobj = () => {
-		if (!tweet || !user) return null;
-		return (
-			<div className="flex space-x-3">
-				{/* ユーザーのアイコン */}
-				<Button
-					className="w-10 h-10 p-0 flex items-center justify-center rounded-full"
-					onClick={hundleUserClick}
-				>
-					<Avatar className="w-full h-full rounded-full">
-						<AvatarImage src={user?.icon_image} alt={user?.userid} />
-						<AvatarFallback>{user?.userid}</AvatarFallback>
-					</Avatar>
-				</Button>
-	
-				{/* ツイートの内容 */}
-				<div className="flex-1 min-w-0">
-					<div className="flex items-start justify-between">
-						<div className="flex items-center space-x-1 truncate">
-							{/* ユーザー名とID */}
-							<button
-								className="text-base font-bold text-gray-900 hover:underline bg-transparent p-0 focus:outline-none"
-								onClick={hundleUserClick}
-							>
-								{user?.username}
-							</button>
-							<button
-								className="text-sm text-gray-500 bg-transparent p-0 focus:outline-none"
-								onClick={hundleUserClick}
-							>
-								@{user?.userid}
-							</button>
-							<span className="text-gray-500 text-sm">·</span>
-							<span className="text-gray-500 text-sm">
-								{formatDate(tweet.created_at)}
-							</span>
-						</div>
-						<Button
-							variant="ghost"
-							size="icon"
-							className="text-gray-500 hover:text-primary"
-						>
-							<MoreHorizontal className="h-5 w-5" />
-						</Button>
-					</div>
-	
-					{/* ツイートのテキスト */}
-					<p className="text-lg text-gray-900 whitespace-pre-wrap">
-						{renderContentWithHashtags(tweet.content)}
-					</p>
-	
-					{/* メディア（画像または動画） */}
-					{tweet.media_url.Valid && (
-						<div className="mt-3 rounded-2xl overflow-hidden border border-gray-200 max-w-[400px]">
-							{tweet.media_url.String.includes("images%") ? (
-								<Image
-									src={tweet.media_url.String}
-									alt="ツイート画像"
-									width={400}
-									height={225}
-									className="w-full h-auto object-cover max-h-[225px]"
-								/>
-							) : tweet.media_url.String.includes("videos%") ? (
-								<video
-									src={tweet.media_url.String}
-									controls
-									className="w-full h-auto object-cover max-h-[225px]"
-								>
-									お使いのブラウザは動画タグをサポートしていません。
-								</video>
-							) : (
-								<p>サポートされていないメディアタイプです。</p>
-							)}
-						</div>
-					)}
-	
-					{/* 引用リツイートされたツイート */}
-					{tweet.isquote && retweet && (
-						<div className="mt-3 mr-10 p-3 border border-gray-200 rounded-lg  hover:bg-gray-100">
-							<RetweetItem tweet={retweet} />
-						</div>
-					)}
-	
-					{/* アクションボタン群 */}
-					<div className="mt-3 flex justify-between max-w-md">
-						<Button
-							variant="ghost"
-							size="sm"
-							className="flex items-center space-x-2 text-gray-500 hover:text-primary"
-							//onClick={handleReplyClick}
-						>
-							<MessageCircle className="h-4 w-4" />
-							<span className="text-xs">{tweet.replies}</span>
-						</Button>
-						<Button
-							variant="ghost"
-							size="sm"
-							className={`flex items-center space-x-2 ${
-								isretweet ? "text-green-500" : "text-gray-500"
-							} hover:text-red-500`}
-							onClick={handleRetweetToggle}
-						>
-							<Repeat className="h-4 w-4" />
-							<span className="text-xs">{retweetData}</span>
-						</Button>
-						<Button
-							variant="ghost"
-							size="sm"
-							className={`flex items-center space-x-2 ${
-								isliked ? "text-red-500" : "text-gray-500"
-							} hover:text-red-500`}
-							onClick={handleLikeToggle}
-						>
-							<Heart
-								className={`h-4 w-4 ${
-									isliked ? "fill-current text-red-500" : "text-gray-500"
-								}`}
-							/>
-							<span className="text-xs">{likeData}</span>
-						</Button>
-						<Button
-							variant="ghost"
-							size="sm"
-							className="flex items-center space-x-2 text-gray-500 hover:text-primary"
-							//onClick={handleImpressionsClick}
-						>
-							<BarChart className="h-4 w-4" />
-							<span className="text-xs">{tweet.impressions}</span>
-						</Button>
-					</div>
-				</div>
-			</div>
-		);
-	};
+    const Tweetobj = () => {
+        if (!tweet || !user) return null;
+        return (
+            <div className="flex space-x-3 flex-col">
+                {/* ユーザーのアイコン */}
+                <div className="flex justify-between">
+					<div className="flex">
+                    <Button
+                        className="w-10 h-10 p-0 flex items-center justify-center rounded-full"
+                        onClick={hundleUserClick}
+                    >
+                        <Avatar className="w-full h-full rounded-full">
+                            <AvatarImage
+                                src={user?.icon_image}
+                                alt={user?.userid}
+                            />
+                            <AvatarFallback>{user?.userid}</AvatarFallback>
+                        </Avatar>
+                    </Button>
 
+                    {/* ツイートの内容 */}
+
+                    <div className="flex space-x-1 truncate flex-col ml-2">
+                        {/* ユーザー名とID */}
+                        <button
+                            className="text-base font-bold text-gray-900 hover:underline bg-transparent p-0 focus:outline-none"
+                            onClick={hundleUserClick}
+                        >
+                            {user?.username}
+                        </button>
+                        <button
+                            className="text-sm text-gray-500 bg-transparent pr-6 focus:outline-none whitespace-nowrap"
+                            onClick={hundleUserClick}
+                        >
+                            @{user?.userid}
+                        </button>
+                    </div>
+					</div>
+                    <Button
+                        variant="ghost"
+                        size="icon"
+                        className="text-gray-500 hover:text-primary"
+                    >
+                        <MoreHorizontal className="h-5 w-5" />
+                    </Button>
+                </div>
+                <div className="flex-1 min-w-0">
+                    {/* ツイートのテキスト */}
+                    <p className="text-lg text-gray-900 whitespace-pre-wrap">
+                        {renderContentWithHashtags(tweet.content)}
+                    </p>
+
+                    {/* メディア（画像または動画） */}
+                    {tweet.media_url.Valid && (
+                        <div className="mt-3 rounded-2xl overflow-hidden border border-gray-200 max-w-[400px]">
+                            {tweet.media_url.String.includes("images%") ? (
+                                <Image
+                                    src={tweet.media_url.String}
+                                    alt="ツイート画像"
+                                    width={400}
+                                    height={225}
+                                    className="w-full h-auto object-cover max-h-[225px]"
+                                />
+                            ) : tweet.media_url.String.includes("videos%") ? (
+                                <video
+                                    src={tweet.media_url.String}
+                                    controls
+                                    className="w-full h-auto object-cover max-h-[225px]"
+                                >
+                                    お使いのブラウザは動画タグをサポートしていません。
+                                </video>
+                            ) : (
+                                <p>サポートされていないメディアタイプです。</p>
+                            )}
+                        </div>
+                    )}
+
+                    {/* 引用リツイートされたツイート */}
+                    {tweet.isquote && retweet && (
+                        <div className="mt-3 mr-10 p-3 border border-gray-200 rounded-lg  hover:bg-gray-100">
+                            <RetweetItem tweet={retweet} />
+                        </div>
+                    )}
+
+                    {/* アクションボタン群 */}
+                    <div className="mt-3 flex justify-between max-w-md">
+                        <Button
+                            variant="ghost"
+                            size="sm"
+                            className="flex items-center space-x-2 text-gray-500 hover:text-primary"
+                            //onClick={handleReplyClick}
+                        >
+                            <MessageCircle className="h-4 w-4" />
+                            <span className="text-xs">{tweet.replies}</span>
+                        </Button>
+                        <Button
+                            variant="ghost"
+                            size="sm"
+                            className={`flex items-center space-x-2 ${
+                                isretweet ? "text-green-500" : "text-gray-500"
+                            } hover:text-red-500`}
+                            onClick={handleRetweetToggle}
+                        >
+                            <Repeat className="h-4 w-4" />
+                            <span className="text-xs">{retweetData}</span>
+                        </Button>
+                        <Button
+                            variant="ghost"
+                            size="sm"
+                            className={`flex items-center space-x-2 ${
+                                isliked ? "text-red-500" : "text-gray-500"
+                            } hover:text-red-500`}
+                            onClick={handleLikeToggle}
+                        >
+                            <Heart
+                                className={`h-4 w-4 ${
+                                    isliked
+                                        ? "fill-current text-red-500"
+                                        : "text-gray-500"
+                                }`}
+                            />
+                            <span className="text-xs">{likeData}</span>
+                        </Button>
+                        <Button
+                            variant="ghost"
+                            size="sm"
+                            className="flex items-center space-x-2 text-gray-500 hover:text-primary"
+                            //onClick={handleImpressionsClick}
+                        >
+                            <BarChart className="h-4 w-4" />
+                            <span className="text-xs">{tweet.impressions}</span>
+                        </Button>
+                    </div>
+                </div>
+            </div>
+        );
+    };
 
     return (
         <div className="flex">
             <Sidebar />
             <div className="min-h-screen bg-white text-black flex-1 ml-80 mr-120 border-l border-r overflow-hidden">
+                <header className="sticky top-0 z-10 bg-white bg-opacity-80 p-4 backdrop-blur-sm">
+                    <div className="flex items-center justify-between">
+                        <div className="flex items-center">
+                            <button
+                                className="mr-4 rounded-full p-2 hover:bg-gray-200"
+                                onClick={() => router.back()}
+                            >
+                                <ArrowLeft size={20} />
+                            </button>
+                            <div>
+                                <h1 className="text-xl font-bold">
+                                    リプライする
+                                </h1>
+                            </div>
+                        </div>
+                    </div>
+                </header>
                 {tweet ? (
                     <div className="p-4 border-b border-gray-200">
-						<Tweetobj />
-					</div>
-                ) : (
-                    <div className="p-4 text-gray-500">
-                        読み込み中...
+                        <Tweetobj />
                     </div>
+                ) : (
+                    <div className="p-4 text-gray-500">読み込み中...</div>
                 )}
             </div>
             <TrendsSidebar />
