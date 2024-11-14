de534321694a183946bfa9964ab7c47e6a76ac35
Namito Hirezaki <7310wave@gmail.com>
2024-11-14 19:22:22 +0900

    update

--- hackathon/src/components/pages/search/index.tsx
+++ hackathon/src/components/pages/search/index.tsx
@@ -1,44 +1,45 @@
-"use client"
+"use client";
 
-import { useState, useEffect,Suspense } from "react";
+import { useState, useEffect, Suspense } from "react";
 import GetFetcher from "@/routes/getfetcher";
 import TweetItem from "@/components/pages/tweet/tweetItems";
 import { TweetData } from "@/types";
-import { useSearchParams,useRouter } from "next/navigation";
+import { useSearchParams, useRouter } from "next/navigation";
 import { ArrowLeft, Search, Settings2 } from "lucide-react";
 
 const SearchPage = () => {
-	const [searchData, setSearchData] = useState<TweetData[]>([]);
-	const router = useRouter();
-	const q = useSearchParams().get("q");
-	const { data: search, error, isLoading } = GetFetcher(
-		`http://localhost:8080/search/${q}`
-	);
+  const [searchData, setSearchData] = useState<TweetData[]>([]);
+  const router = useRouter();
+  const q = useSearchParams().get("q"); // search paramsはSuspense外で使う
 
-	useEffect(() => {
-		if (search) {
-			setSearchData(search);
-		}
-	}, [search]);
+  const { data: search, error, isLoading } = GetFetcher(
+    `http://localhost:8080/search/${q}`
+  );
 
-	if (isLoading) {
-		return (
-			<div className="flex min-h-screen items-center justify-center bg-white text-black">
-				<p>読み込み中...</p>
-			</div>
-		);
-	}
-	if (error) {
-		return (
-			<div className="flex min-h-screen items-center justify-center bg-white text-black">
-				<p>再読み込みしてください</p>
-			</div>
-		);
-	}
+  useEffect(() => {
+    if (search) {
+      setSearchData(search);
+    }
+  }, [search]);
 
-	return (
-		<Suspense fallback={<div>Loading...</div>}>
-			<header className="sticky top-0 z-10 border-b bg-white/80 backdrop-blur-sm">
+  if (isLoading) {
+    return (
+      <div className="flex min-h-screen items-center justify-center bg-white text-black">
+        <p>読み込み中...</p>
+      </div>
+    );
+  }
+  if (error) {
+    return (
+      <div className="flex min-h-screen items-center justify-center bg-white text-black">
+        <p>再読み込みしてください</p>
+      </div>
+    );
+  }
+
+  return (
+    <>
+      <header className="sticky top-0 z-10 border-b bg-white/80 backdrop-blur-sm">
         <div className="flex items-center gap-4 p-4">
           <button
             onClick={() => router.back()}
@@ -52,7 +53,6 @@ const SearchPage = () => {
               type="search"
               placeholder="検索"
               className="w-full rounded-full bg-gray-100 py-2 pl-10 pr-4 focus:bg-white focus:outline-none focus:ring-2 focus:ring-blue-500"
-              //onChange={(e) => handleSearch(e.target.value)}
             />
             <Search className="absolute left-3 top-1/2 h-5 w-5 -translate-y-1/2 text-gray-500" />
           </div>
@@ -64,20 +64,24 @@ const SearchPage = () => {
           </button>
         </div>
       </header>
-			<div>
-				{searchData.map((data, index) => (
-					<TweetItem
-					key={index}
-					type={"tweet"}
-					tweet={data.tweet}
-					user={data.user}
-					initialisLiked={data.likes}
-					initialisRetweeted={data.retweets}
-				/>
-				))}
-			</div>
-		</Suspense>
-	);
-}
+
+      {/* Suspenseはデータの取得にのみ使用 */}
+      <Suspense fallback={<div>Loading...</div>}>
+        <div>
+          {searchData.map((data, index) => (
+            <TweetItem
+              key={index}
+              type={"tweet"}
+              tweet={data.tweet}
+              user={data.user}
+              initialisLiked={data.likes}
+              initialisRetweeted={data.retweets}
+            />
+          ))}
+        </div>
+      </Suspense>
+    </>
+  );
+};
 
 export default SearchPage;
