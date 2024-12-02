0adc44f3808b29b071ff241fd44ebef4086d23a9
Namito Hirezaki <7310wave@gmail.com>
2024-12-02 19:41:10 +0900

    update

--- hackathon/src/app/page.tsx
+++ hackathon/src/app/page.tsx
@@ -5,7 +5,6 @@ import { LoginForm } from "@/components/pages/login";
 export default function Home() {
     return (
         <div>
-            {/* ログインしていない場合はLoginFormを表示 */}
             <LoginForm />
         </div>
     );
