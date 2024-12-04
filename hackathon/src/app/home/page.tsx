9e38b0d8a4018bac34e35b2cac384f5d955b4adc
Namito Hirezaki <7310wave@gmail.com>
2024-12-04 18:49:53 +0900

    update

--- hackathon/src/app/home/page.tsx
+++ hackathon/src/app/home/page.tsx
@@ -2,11 +2,14 @@
 
 import Layout from "@/components/pages/layout/layout"
 import HomePage from "@/components/pages/home"
+import { Suspense } from "react"
 
 export default function Component () {
 	return (
 		<Layout>
+			<Suspense fallback={<div>Loading...</div>}>
 			<HomePage />
+			</Suspense>
 		</Layout>
 	)
 }
