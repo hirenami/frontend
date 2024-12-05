3e44aa61a8e3d4e95e44a0491edcaac307b91df9
Namito Hirezaki <7310wave@gmail.com>
2024-12-05 21:47:05 +0900

    update

--- hackathon/src/app/message/page.tsx
+++ hackathon/src/app/message/page.tsx
@@ -1,5 +1,6 @@
 "use client";
 
+import MobileNavigation from "@/components/pages/layout/components/mobilesidebar";
 import Sidebar from "@/components/pages/layout/components/sidebar";
 import DirectMessage from "@/components/pages/message";
 
@@ -7,9 +8,10 @@ export default function Component() {
     return (
 		<div className="flex min-h-screen bg-background">
 			<Sidebar />
-			<main className="flex-1 ml-0 md:ml-80 mr-0 md:mr-120 border-r border-l">
+			<main className="flex-1 ml-0 md:ml-80 mr-0 md:mr-10 border-r border-l">
 				<DirectMessage />
 			</main>
+			<MobileNavigation />
 		</div>
     );
 }
