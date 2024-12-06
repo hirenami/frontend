777145c36d61bae7b37cba06d1ad82f641e59587
Namito Hirezaki <7310wave@gmail.com>
2024-12-07 01:01:38 +0900

    updste

--- hackathon/src/app/home/page.tsx
+++ hackathon/src/app/home/page.tsx
@@ -2,9 +2,26 @@
 
 import Layout from "@/components/pages/layout/layout"
 import HomePage from "@/components/pages/home"
-import { Suspense } from "react"
+import { Suspense, useEffect } from "react"
+import { useRouter } from "next/navigation"
+import { fireAuth } from "@/features/firebase/auth"
+import { onAuthStateChanged } from "firebase/auth"
 
 export default function Component () {
+	const router = useRouter();
+	useEffect(() => {
+        const auth = fireAuth;
+        // ユーザーの認証状態を監視
+        const unsubscribe = onAuthStateChanged(auth, (user) => {
+			if (!user) {
+				// ログインしている場合はホーム画面に遷移
+				router.push("/login");
+			}
+		});
+		return () => unsubscribe();
+	}
+	, [router]);
+
 	return (
 		<Layout>
 			<Suspense >
